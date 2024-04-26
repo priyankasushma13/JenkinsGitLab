a07698816ee91ae2a2ae4c741694c99013c91b26
Sushma Priyanka <78401534+priyankasushma13@users.noreply.github.com>
2024-04-26 12:30:12 +0530

    Update JenkinsLab.java

--- JenkinsLab.java
+++ JenkinsLab.java
@@ -1,5 +1,5 @@
 public class JenkinsLab{
     public static void main(String[] args){
-        System.out.println(x:"Author:Dakshinya - GitHub Jenkins Build demo");
+        System.out.println("Author:Dakshinya - GitHub Jenkins Build demo");
     }
 }
